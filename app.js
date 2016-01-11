da98a7abf753b766bfdce5d0c421ab70beadd87d
Rahul Pisal <rpisal@gmail.com>
2016-01-11 10:25:03 +0400

    prototypal inheritance and function constructors

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var person = {
-  firstname: 'John',
-  lastname: 'Doe',
-  greet: function() {
-      console.log('Hello ' + this.firstname + ' ' + this.lastname);
-  }  
-};
+function Person(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
 
-person.greet();
+Person.prototype.greet = function() {
+    console.log('Hello ' + this.firstname + ' ' + this.lastname);
+}
 
-console.log(person['firstname']);
-console.log(person.lastname);
+var john = new Person('John', 'Doe');
+john.greet();
+
+var jane = new Person('Jane', 'Doe');
+jane.greet();
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
